1ef9a2f4510c5b0db464de65cb9f5a0e4a2a000d
Karlmarl1990 <mykarlmarl@gmx.net>
2025-12-17 06:50:54 +0100

    Create prepare_vscode.sh for environment setup
    This script sets up environment variables for the Alkemi application.

---
+++ prepare_vscode.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+echo "Starting Clean Prepare Script..."
+export APP_NAME="Alkemi"
+export APP_NAME_LC="alkemi"
+export BINARY_NAME="alkemi"
+export GH_REPO_PATH="Karlmarl1990/Alkemi-IDE"
+export ORG_NAME="Alkemi"
+export TUNNEL_APP_NAME="alkemi-tunnel"
+echo "Variables set successfully. Moving to build stage."
